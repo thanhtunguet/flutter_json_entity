9621042976b30846f37003f839950b21dceae52c
thanhtunguet <thanhtung.uet@gmail.com>
2024-07-09 14:24:04 +0700

    add cookies manager

--- lib/services/cookie_storage_service.dart
+++ lib/services/cookie_storage_service.dart
@@ -1,6 +1,7 @@
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:supa_architecture/supa_architecture.dart';
 
 abstract interface class CookieStorageService {
   static Future<CookieStorageService> initialize() async {
@@ -16,10 +17,20 @@ abstract interface class CookieStorageService {
   }
 
   CookieManager getCookieManager();
+
+  Future<List<Cookie>> getCookies();
+
+  Future<void> setCookies(List<Cookie> cookies);
 }
 
 class _CookieStorageServiceImpl implements CookieStorageService {
   final PersistCookieJar persistCookieJar;
+
+  static const _authenticationPath = '/rpc/portal/authentication';
+
+  static Uri get _authenticationUri =>
+      Uri.parse(persistentStorageService.baseApiUrl)
+          .replace(path: _authenticationPath);
 
   _CookieStorageServiceImpl._({
     required this.persistCookieJar,
@@ -28,5 +39,21 @@ class _CookieStorageServiceImpl implements CookieStorageService {
   @override
   CookieManager getCookieManager() {
     return CookieManager(persistCookieJar);
+  }
+
+  @override
+  Future<List<Cookie>> getCookies() async {
+    return persistCookieJar.loadForRequest(_authenticationUri);
+  }
+
+  @override
+  Future<void> setCookies(
+    List<Cookie> cookies, {
+    bool deleteAll = false,
+  }) async {
+    if (deleteAll) {
+      await persistCookieJar.deleteAll();
+    }
+    return persistCookieJar.saveFromResponse(_authenticationUri, cookies);
   }
 }
