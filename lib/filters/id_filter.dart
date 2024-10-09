2e72543831e11996eff58d51b1556208344287f9
thanhtunguet <thanhtung.uet@gmail.com>
2024-10-09 10:23:05 +0700

    id filter int

--- lib/filters/id_filter.dart
+++ lib/filters/id_filter.dart
@@ -2,7 +2,7 @@ part of 'filters.dart';
 
 /// This class defines filter operations for ID (primary key, foreign key) fields
 /// that is in int type
-class IdFilter extends AbstractIdFilter<num> {
+class IdFilter extends AbstractIdFilter<int> {
   /// This class defines filter operations for ID (primary key, foreign key) fields
   /// that is in int type.
   ///
