40fab64e00bc989f9676e66bcd3889c89f4adf6b
thanhtunguet <thanhtung.uet@gmail.com>
2025-02-04 22:24:03 +0700

    feat: v1.1.3 add getter

--- lib/supa_architecture.dart
+++ lib/supa_architecture.dart
@@ -1,5 +1,8 @@
 library supa_architecture;
 
+import 'package:supa_architecture/core/cookie_manager/cookie_manager.dart';
+import 'package:supa_architecture/core/persistent_storage/persistent_storage.dart';
+
 import 'supa_architecture_platform_interface.dart';
 
 export 'api_client/api_client.dart';
@@ -21,3 +24,9 @@ class SupaArchitecture {
     return SupaArchitecturePlatform.instance.getPlatformVersion();
   }
 }
+
+PersistentStorage get persistentStorage =>
+    SupaArchitecturePlatform.instance.persistentStorage;
+
+CookieManager get cookieManager =>
+    SupaArchitecturePlatform.instance.cookieStorage;
