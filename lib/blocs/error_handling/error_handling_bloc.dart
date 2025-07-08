ef7a3e18f32bf381fa5718a7a66cba5b3051b242
thanhtunguet <thanhtung.uet@gmail.com>
2025-07-08 14:28:04 +0700

    Refactor error handling in ErrorHandlingBloc to comment out FirebaseCrashlytics integration for uncaught errors, platform errors, and DioExceptions, simplifying the error logging process.

--- lib/blocs/error_handling/error_handling_bloc.dart
+++ lib/blocs/error_handling/error_handling_bloc.dart
@@ -1,6 +1,5 @@
 import "package:bloc/bloc.dart";
 import "package:dio/dio.dart";
-import "package:firebase_crashlytics/firebase_crashlytics.dart";
 import "package:flutter/foundation.dart";
 import "package:supa_architecture/supa_architecture_platform_interface.dart";
 
@@ -24,15 +23,15 @@ class ErrorHandlingBloc extends Cubit<void> {
     // Pass all uncaught errors to Crashlytics
     FlutterError.onError = (errorDetails) {
       if (SupaArchitecturePlatform.instance.useFirebase) {
-        FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-        FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+        // FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+        // FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
       }
     };
 
     // Pass all platform dispatcher errors to Crashlytics
     PlatformDispatcher.instance.onError = (error, stack) {
       if (SupaArchitecturePlatform.instance.useFirebase) {
-        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+        // FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
       }
       return true;
     };
@@ -48,14 +47,14 @@ class ErrorHandlingBloc extends Cubit<void> {
     if (error is Error) {
       // Log to Firebase Crashlytics on supported platforms (Android, iOS)
       if (!kIsWeb && SupaArchitecturePlatform.instance.useFirebase) {
-        FirebaseCrashlytics.instance.recordError(error, error.stackTrace);
+        // FirebaseCrashlytics.instance.recordError(error, error.stackTrace);
       }
       // Log to Sentry for web and all other platforms
       _captureException(error);
     } else if (error is DioException) {
       // Handle DioException separately
       if (!kIsWeb && SupaArchitecturePlatform.instance.useFirebase) {
-        FirebaseCrashlytics.instance.recordError(error, error.stackTrace);
+        // FirebaseCrashlytics.instance.recordError(error, error.stackTrace);
       }
       _captureException(error);
     } else {
@@ -65,10 +64,6 @@ class ErrorHandlingBloc extends Cubit<void> {
   }
 
   void _captureException(dynamic error) {
-    if (!kIsWeb) {
-      if (SupaArchitecturePlatform.instance.useFirebase) {
-        FirebaseCrashlytics.instance.recordError(error, error.stackTrace);
-      }
-    }
+    // Do nothing
   }
 }
